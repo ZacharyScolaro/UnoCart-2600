3530fac8a324e6f39beea2b08ff7e8c94e4789d3
Sven Oliver Moll <svolli@svolli.de>
2019-06-03 21:25:40 +0200

    addendum: rename already existing header to work on Linux
    Signed-off-by: Sven Oliver Moll <svolli@svolli.de>

--- source/STM32firmware/Atari2600Cart/src/cartridge_ace.h
+++ source/STM32firmware/Atari2600Cart/src/cartridge_ace.h
@@ -1,6 +1,8 @@
+#ifndef CARTRIDGE_ACE_H
+#define CARTRIDGE_ACE_H
 
-#ifndef CARTRIDGE_ACE
-#define CARTRIDGE_ACE CARTRIDGE_ACE
 int is_ace_cartridge(unsigned int image_size, uint8_t *buffer);
+
 int launch_ace_cartridge(const char* filename, uint32_t buffer_size, uint8_t *buffer);
-#endif
+
+#endif // CARTRIDGE_SUPERCHARGER_H
